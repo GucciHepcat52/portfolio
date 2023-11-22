bfa4b8654baa4d2c7719ea3ff739a06ec5b60401
Dallin Breen <dallinbreen624@gmail.com>
2023-11-22 13:17:57 -0700

    Styling landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+import "./Home.css";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className="main">
+      <div className="top">
+        <h1>Top</h1>
+      </div>
+      <div className="bottom">
+        <div className="section" id="software">
+          <button>Software</button>
+        </div>
+        <div className="section" id="fitness">
+          <button>Fitness</button>
+        </div>
+        <div className="section" id="gaming">
+          <button>Gaming</button>
+        </div>
+      </div>
     </div>
   );
 }
