2b20cadb8e81149b391c10e4e246e57c6dc3e899
Dallin Breen <dallinbreen624@gmail.com>
2024-02-19 15:12:48 -0700

    restarting app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "../src/components/Home/Home";
-import SoftwareDevelopment from "../src/components/SoftwareDevelopment/SoftwareDevelopment";
-import Fitness from "../src/components/Fitness/Fitness";
-import Gaming from "../src/components/Gaming/Gaming";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/portfolio" element={<Home />} />
-        <Route path="/portfolio/software" element={<SoftwareDevelopment />} />
-        <Route path="/portfolio/fitness" element={<Fitness />} />
-        <Route path="/portfolio/gaming" element={<Gaming />} />
-      </Routes>
+      <h1>Portfolio Page</h1>
     </div>
   );
 }
