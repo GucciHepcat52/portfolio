5365e2ff065a181acc80d4323e4f5fb36f37ef42
Dallin Breen <dallinbreen624@gmail.com>
2024-02-20 11:13:46 -0700

    added scrolling to view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import "./App.css";
 
 function App() {
+  const technologyRef = useRef(null);
+  const projectsRef = useRef(null);
+  const aboutRef = useRef(null);
+  const contactRef = useRef(null);
+
+  function scrollToRef(ref) {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <div className="App">
       <section className="hero">
@@ -17,19 +26,19 @@ function App() {
           <div className="hero-links"></div>
         </div>
       </section>
-      <section className="technology">
+      <section className="technology" ref={technologyRef}>
         <h1>Technology</h1>
         <div className="technology-container"></div>
       </section>
-      <section className="projects">
+      <section className="projects" ref={projectsRef}>
         <h1>Projects</h1>
         <div className="project"></div>
       </section>
-      <section className="about-me">
+      <section className="about-me" ref={aboutRef}>
         <h1>About Me</h1>
         <div className="about-me-container"></div>
       </section>
-      <section className="contact-me">
+      <section className="contact-me" ref={contactRef}>
         <h1>Contact Me</h1>
         <div className="contact-container"></div>
       </section>
