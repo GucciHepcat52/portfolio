9ae2df6ea30a0e890ca17faa0959e9fbf4bb546f
Dallin Breen <dallinbreen624@gmail.com>
2024-02-19 17:31:01 -0700

    updated layout of portfolio page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,34 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio Page</h1>
+      <section className="hero">
+        <header></header>
+        <div className="hero-inner">
+          <p className="hero-subheading">Hi, I'm Dallin Breen.</p>
+          <div className="hero-heading"></div>
+          <p className="hero-intro">
+            I build effective and efficient technology, designed to help the
+            world become a better place!
+          </p>
+          <div className="hero-links"></div>
+        </div>
+      </section>
+      <section className="technology">
+        <h1>Technology</h1>
+        <div className="technology-container"></div>
+      </section>
+      <section className="projects">
+        <h1>Projects</h1>
+        <div className="project"></div>
+      </section>
+      <section className="about-me">
+        <h1>About Me</h1>
+        <div className="about-me-container"></div>
+      </section>
+      <section className="contact-me">
+        <h1>Contact Me</h1>
+        <div className="contact-container"></div>
+      </section>
     </div>
   );
 }
